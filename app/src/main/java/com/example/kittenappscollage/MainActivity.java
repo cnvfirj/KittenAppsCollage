968a1efde47d0c6147cfc547b7b2eb24350f03ab
cnvfirj <cnvfirj@gmail.com>
2019-07-08 10:20:14 +0300

    upgrade in work

--- app/src/main/java/com/example/kittenappscollage/MainActivity.java
+++ app/src/main/java/com/example/kittenappscollage/MainActivity.java
@@ -6,14 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 9;
     }
 
-    private void aVoid(){
-
-    }
 }
