ad76f79e3f84c4582962bfe6b249085521b78c3f
cnvfirj <cnvfirj@gmail.com>
2019-07-10 16:41:09 +0300

    add dependencies in Work

--- app/src/main/java/com/example/kittenappscollage/helpers/Massages.java
+++ app/src/main/java/com/example/kittenappscollage/helpers/Massages.java
@@ -7,15 +7,15 @@ import android.util.Log;
 public class Massages {
     private static final String TAG = "MASSAGE APP";
 
-    public static void ERROR(String text, Context c){
+    public static void ERROR(String text, Class c){
         Log.e(TAG,"class name - "+getName(c)+" error - "+text);
     }
 
-    public static void MASSAGE(String text, Context c){
+    public static void MASSAGE(String text, Class c){
         Log.d(TAG,"class name - "+getName(c)+" massage - "+text);
     }
 
-    private static String getName(Context c){
+    private static String getName(Class c){
         if (c!=null) return c.getClass().getName();
         else return "anon class";
     }
