3a2c952155b2ffd485fcb3ae619573bb0935217e
cnvfirj <cnvfirj@gmail.com>
2019-07-11 14:42:53 +0300

    add FragmentDraw in Work

--- app/src/main/java/com/example/kittenappscollage/draw/FragmentDraw.java
+++ app/src/main/java/com/example/kittenappscollage/draw/FragmentDraw.java
@@ -10,16 +10,38 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.kittenappscollage.R;
+import com.example.kittenappscollage.packProj.RepDraw;
+
 public class FragmentDraw extends Fragment {
+
+    private ViewDraw dViewDraw;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_draw,null);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        dViewDraw = view.findViewById(R.id.view_draw);
+        RepDraw.get().list(new RepDraw.Adding() {
+            @Override
+            public void readinessImg(boolean is) {
+                dViewDraw.applyMutable();
+            }
+
+            @Override
+            public void readinessLyr(boolean is) {
+                dViewDraw.applyMutable();
+            }
+
+            @Override
+            public void readinessAll(boolean is) {
+                dViewDraw.applyMutable();
+            }
+        });
     }
 }
