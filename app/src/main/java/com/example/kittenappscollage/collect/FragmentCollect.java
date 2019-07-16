b2156bda68a82fa95fd8240d0490c5eb890217af
cnvfirj <cnvfirj@gmail.com>
2019-07-16 14:35:31 +0300

    add ViewPager

--- app/src/main/java/com/example/kittenappscollage/collect/FragmentCollect.java
+++ app/src/main/java/com/example/kittenappscollage/collect/FragmentCollect.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.kittenappscollage.R;
 
 public class FragmentCollect extends Fragment {
@@ -22,5 +25,8 @@ public class FragmentCollect extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Glide.with(this)
+                .load(R.drawable.ic_star)
+                .into((ImageView) view.findViewById(R.id.ic_account));
     }
 }
