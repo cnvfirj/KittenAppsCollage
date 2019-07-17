f9ae2ec12bc2c9801e9153a2653f24abf7ed417e
cnvfirj <cnvfirj@gmail.com>
2019-07-17 11:29:40 +0300

    add tools draw

--- app/src/main/java/com/example/kittenappscollage/draw/DrawTools.java
+++ app/src/main/java/com/example/kittenappscollage/draw/DrawTools.java
@@ -9,12 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.kittenappscollage.R;
+
 public class DrawTools extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.draw_tools,null);
     }
 
 
