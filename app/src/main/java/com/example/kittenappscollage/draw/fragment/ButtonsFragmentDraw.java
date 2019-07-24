be1c07839fec9ca519cb1d7b3f978ceb4ad088e9
cnvfirj <cnvfirj@gmail.com>
2019-07-24 14:37:57 +0300

    add slide save and adding in work

--- app/src/main/java/com/example/kittenappscollage/draw/fragment/ButtonsFragmentDraw.java
+++ app/src/main/java/com/example/kittenappscollage/draw/fragment/ButtonsFragmentDraw.java
@@ -1,4 +1,34 @@
 package com.example.kittenappscollage.draw.fragment;
 
 public class ButtonsFragmentDraw extends SuperFragmentDraw {
+
+    @Override
+    protected void saveNet() {
+        super.saveNet();
+    }
+
+    @Override
+    protected void saveTel() {
+        super.saveTel();
+    }
+
+    @Override
+    protected void addCreated() {
+        super.addCreated();
+    }
+
+    @Override
+    protected void addLink() {
+        super.addLink();
+    }
+
+    @Override
+    protected void addCam() {
+        super.addCam();
+    }
+
+    @Override
+    protected void addColl() {
+        super.addColl();
+    }
 }
