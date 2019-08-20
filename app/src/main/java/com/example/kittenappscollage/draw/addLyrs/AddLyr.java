10db8dc404c70d9326ac2083894e5295257e954f
cnvfirj <cnvfirj@gmail.com>
2019-08-20 15:13:56 +0300

    add fon in draw

--- app/src/main/java/com/example/kittenappscollage/draw/addLyrs/AddLyr.java
+++ app/src/main/java/com/example/kittenappscollage/draw/addLyrs/AddLyr.java
@@ -1,8 +1,32 @@
 package com.example.kittenappscollage.draw.addLyrs;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class AddLyr extends DialogFragment {
 
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
 }
