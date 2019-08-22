13a600fa63dd84be676bff07208ec522235d186b
cnvfirj <cnvfirj@gmail.com>
2019-08-22 16:43:19 +0300

    add example new SeekBar

--- app/src/main/java/com/example/kittenappscollage/draw/addLyrs/AddLyr.java
+++ app/src/main/java/com/example/kittenappscollage/draw/addLyrs/AddLyr.java
@@ -1,25 +1,23 @@
 package com.example.kittenappscollage.draw.addLyrs;
 
-import android.app.Dialog;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.example.kittenappscollage.R;
 
 import static com.example.kittenappscollage.helpers.Massages.MASSAGE;
 
-public class AddLyr extends Fragment {
+public class AddLyr extends Fragment implements View.OnClickListener {
+
+
+    private ImageView aBack, aDone, aClose, aMirror;
 
 
     @Nullable
@@ -30,11 +28,40 @@ public class AddLyr extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+    private void initViews(View v){
+        aBack = v.findViewById(R.id.dialog_back);
+        aBack.setOnClickListener(this);
+        aDone = v.findViewById(R.id.dialog_apply);
+        aDone.setOnClickListener(this);
+        aClose = v.findViewById(R.id.dialog_close);
+        aClose.setOnClickListener(this);
+        aMirror = v.findViewById(R.id.dialog_mirror);
+        aMirror.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.dialog_back:
+                pressBack((ImageView) view);
+                break;
+            case R.id.dialog_apply:
+                pressDone((ImageView) view);
+                break;
+            case R.id.dialog_close:
+                pressClose((ImageView) view);
+                break;
+            case R.id.dialog_mirror:
+                pressMirror((ImageView) view);
+                break;
+
+        }
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +73,24 @@ public class AddLyr extends Fragment {
         super.onResume();
 
     }
+
+    private void pressMirror(ImageView view){
+        view.setSelected(!view.isSelected());
+    }
+
+    private void pressDone(ImageView view){
+        view.setSelected(!view.isSelected());
+    }
+
+    private void pressClose(ImageView view){
+        view.setSelected(!view.isSelected());
+    }
+
+    private void pressBack(ImageView view){
+        view.setSelected(!view.isSelected());
+    }
+
+
 
 
 
