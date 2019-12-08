6e5821773a9b0d6eae662bdd27182f0a2cc907ba
Ihor <cnvfirj@gmail.com>
2019-12-08 12:46:06 +0300

    work

--- app/src/main/java/com/example/kittenappscollage/draw/repozitoryDraw/RepCommonFunctions.java
+++ app/src/main/java/com/example/kittenappscollage/draw/repozitoryDraw/RepCommonFunctions.java
@@ -8,7 +8,6 @@ import com.example.kittenappscollage.draw.operations.bitmap.CoercionBitmap;
 import com.example.kittenappscollage.draw.operations.bitmap.DrawBitmap;
 import com.example.kittenappscollage.draw.saveSteps.BackNextStep;
 import com.example.kittenappscollage.helpers.App;
-import com.example.mutmatrix.CompRep;
 import com.example.mutmatrix.DeformMat;
 
 public class RepCommonFunctions extends RepParams {
@@ -104,19 +103,19 @@ public class RepCommonFunctions extends RepParams {
 
     public void correctLyr(){
         correctOpLyr();
-        if(isLyr()){
-            Bitmap temp = CoercionBitmap.blankBitmap(rLyrMat);
-            CoercionBitmap.drawBitmap(new Canvas(temp), CoercionBitmap.matrixBitmap(rLyrMat), rLyr);
-            rLyr = temp.copy(Bitmap.Config.ARGB_8888, true);
-            rLyrC = new Canvas(rImg);
-            float scale = rLyrMat.getRepository().getScale();
-            PointF translate = CoercionBitmap.transBitmap(rLyrMat);
-            rLyrMat.reset();
-            rLyrMat.view(rView).bitmap(new PointF(temp.getWidth(), temp.getHeight()));
-            rLyrMat.getRepository().setScale(scale);
-            rLyrMat.getRepository().setTranslate(translate);
-            zeroingBitmap(temp);
-        }
+//        if(isLyr()){
+//            Bitmap temp = CoercionBitmap.blankBitmap(rLyrMat);
+//            CoercionBitmap.drawBitmap(new Canvas(temp), CoercionBitmap.matrixBitmap(rLyrMat), rLyr);
+//            rLyr = temp.copy(Bitmap.Config.ARGB_8888, true);
+//            rLyrC = new Canvas(rImg);
+//            float scale = rLyrMat.getRepository().getScale();
+//            PointF translate = CoercionBitmap.transBitmap(rLyrMat);
+//            rLyrMat.reset();
+//            rLyrMat.view(rView).bitmap(new PointF(temp.getWidth(), temp.getHeight()));
+//            rLyrMat.getRepository().setScale(scale);
+//            rLyrMat.getRepository().setTranslate(translate);
+//            zeroingBitmap(temp);
+//        }
     }
 
     private void correctOpLyr(){
@@ -124,7 +123,7 @@ public class RepCommonFunctions extends RepParams {
             Bitmap temp = CoercionBitmap.correctBlank(rLyrMat);
             PointF trans = CoercionBitmap.correctTrans(rLyrMat);
             DeformMat mat = new DeformMat(App.getMain());
-            mat.view(rView).bitmap(new PointF(temp.getWidth(), temp.getHeight())).getRepository().setScale(rImgMat.getRepository().getScale());
+            mat.view(rView).bitmap(new PointF(temp.getWidth(), temp.getHeight())).getRepository().setScale(rLyrMat.getRepository().getScale());
             mat.getRepository().setTranslate(trans);
             DrawBitmap.create(new Canvas(temp),mat).draw(rLyr, rLyrMat);
             rLyr = temp.copy(Bitmap.Config.ARGB_8888, true);
