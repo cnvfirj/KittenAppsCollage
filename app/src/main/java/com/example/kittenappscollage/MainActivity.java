9ef6023842236414d601e122f8ebc710211a28f0
cnvfirj <cnvfirj@gmail.com>
2020-04-10 14:45:47 +0300

    add splash window

--- app/src/main/java/com/example/kittenappscollage/MainActivity.java
+++ app/src/main/java/com/example/kittenappscollage/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements DialogLoadOldProj
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WorkDBPerms.get(getApplicationContext());
